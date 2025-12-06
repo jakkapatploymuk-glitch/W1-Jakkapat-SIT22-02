d1fb3bc18491f578318d07eebdf7ebb23204d550
jakkapat-sit22-2 <jakkapat.ploymuk@e-tech.ac.th>
2025-12-06 10:51:49 +0700

    update_commit_

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyHomePage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(20),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
+                        color: Colors.grey.withOpacity(0.5 ),
                         spreadRadius: 2,
                         blurRadius: 7,
                         offset: const Offset(0, 3), // changes position of shadow
